6ce043fe2c449285504a8bc9a9eee99e7c09052e
name gos here <email gos here>
2025-08-20 12:26:23 +1000

    Production Database Chnage

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,29 +1,19 @@
-#from sqlalchemy import create_engine
-#from sqlalchemy.orm import sessionmaker
-#from sqlalchemy.ext.declarative import declarative_base
-
-#QLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
-
-#engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
-
-#SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-#Base = declarative_base()
-
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Replace with your real Supabase connection string
-SQLALCHEMY_DATABASE_URL = (
-    "postgresql+psycopg2://postgres:Admin%40thongdy090@"
-    "db.dvxmpvzsbbalwfxwvhis.supabase.co:5432/postgres"
-)
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+QLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+# Replace with your real connection string
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:Admin%40thongdy090@db.dvxmpvzsbbalwfxwvhis.supabase.co:5432/postgres"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
 
